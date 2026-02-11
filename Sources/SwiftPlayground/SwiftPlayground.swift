ec1e7b858f16377c990375635ff877daa3353d33
Giraffe <josh.otoole@student.onslow.school.nz>
2026-02-11 21:33:19 +0000

    Functions on my meat

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,9 +1,30 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+
+func printRoomIntro() {
+    print("This program calculates room area and volume.")
+    print("It also finds the usable space after furniture.")
+}
+
+func printDivider() {
+    print("--------")
+}
+
+func printUnitsNotes() {
+    print("All measurements are in metres.")
+}
+func printGoodbye() {
+    print("Done. Thanks for using the calc.")
+}
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+        printRoomIntro()
+        printDivider()
+        print("Summary here")
+        printDivider()
+        printUnitsNotes()
+        printGoodbye()
     }
 }
